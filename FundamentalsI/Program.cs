e0298e7b80f2587c1a831e80d4d2590042b7722d
Drew Anthony <39780586+drew-anthony@users.noreply.github.com>
2018-09-04 16:14:58 -0500

    Add files via upload

---
+++ FundamentalsI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FundamentalsI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i<=255; i++){
+                Console.WriteLine(i);
+            }
+            for (int i = 1; i<=100; i++){
+                if (i%3==0 && i%5!=0 || i%3!=0 && i%5==0){
+                    Console.WriteLine(i);
+                }
+            }
+            for (int i = 1; i<=100; i++){
+                if (i%3==0 && i%5!=0){
+                    Console.WriteLine("Fizz");
+                } 
+                if (i%3!=0 && i%5==0){
+                    Console.WriteLine("Buzz");
+                }
+                if (i%3==0 && i%5==0){
+                    Console.WriteLine("FizzBuzz");
+                }
+            }
+        }
+    }
+}
