6e577cd8b306226ce9d6128c295980d38a680feb
Drew Anthony <39780586+drew-anthony@users.noreply.github.com>
2018-09-06 15:03:55 -0500

    Add files via upload

---
+++ PortfolioI/Controllers/HelloControllers.cs
@@ -0,0 +1,26 @@
+    using Microsoft.AspNetCore.Mvc;
+    namespace YourNamespace.Controllers     //be sure to use your own project's namespace!
+    {
+        public class HelloController : Controller   //remember inheritance??
+        {
+            //for each route this controller is to handle:
+            [HttpGet]       //type of request
+            [Route("")]     //associated route string (exclude the leading /)
+            public string Index()
+            {
+                return "Hello World from HelloController!";
+            }
+            [HttpGet]       //type of request
+            [Route("projects")]     //associated route string (exclude the leading /)
+            public string Projects()
+            {
+                return "These are my projects";
+            }
+            [HttpGet]       //type of request
+            [Route("contact")]     //associated route string (exclude the leading /)
+            public string Contact()
+            {
+                return "This is my contact";
+            }
+        }
+    }
