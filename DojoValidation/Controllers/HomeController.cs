7eb4c7ed7b312f88da7e93d918cd37093736f765
Drew Anthony <39780586+drew-anthony@users.noreply.github.com>
2018-09-10 08:29:26 -0500

    Add files via upload

---
+++ DojoValidation/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DojoValidation.Models;
+
+namespace DojoValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create(Ninja ninja)
+        {
+            if(ModelState.IsValid)
+            {
+               return RedirectToAction("Result", ninja);
+            }
+            else
+            {           
+                return View("Index",ninja);
+            }
+        }
+        public IActionResult Result(Ninja ninja)
+        {
+            return View("Result", ninja);
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DojoValidation/Models/Ninja.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DojoValidation.Models 
+{
+    public class Ninja {
+
+        [Required]
+        [MinLength(3),MaxLength(15)]
+        public string Name {get;set;}
+
+        [Required]
+        [MinLength(3)]
+        public string Location {get;set;}
+
+        [Required]
+        [MinLength(3),MaxLength(15)]
+        public string Language {get;set;}
+
+        
+        [MinLength(8),MaxLength(30)]
+        public string Comment {get;set;}
+    }
+}
