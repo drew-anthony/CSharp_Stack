346bdd42d1dc99dd3cc3b0c39b86481e5e076d67
Drew Anthony <39780586+drew-anthony@users.noreply.github.com>
2018-09-11 10:35:00 -0500

    Add files via upload

---
+++ QuotingDojo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuotingDojo.Models;
+using System.Web;
+using DbConnection;
+
+namespace QuotingDojo.Controllers
+{
+    public class HomeController : Controller
+    {  
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("")]
+        public IActionResult Quotes()
+        {
+            DateTime CurrentTime  = DateTime.Now;
+             List<Dictionary<string, object>> AllUsers = DbConnector.Query("SELECT * FROM quotes");
+            ViewBag.AllUsers = AllUsers;
+                        return View();
+
+            }
+        [HttpPost("quotes")]
+        public IActionResult Quotes(string djquotes, string quotext)
+        {
+            DateTime CurrentTime  = DateTime.Now;
+            System.Console.WriteLine(djquotes);
+            System.Console.WriteLine(quotext);
+            string date = CurrentTime.ToString("yyyy-MM-dd HH:mm:ss");
+            string query = $"INSERT INTO quotes (quote, quoter, created_at) VALUES ('{quotext}','{djquotes}', '{date}')";
+            DbConnector.Execute(query);
+            return RedirectToAction("Index");
+        }
+    }
+}
